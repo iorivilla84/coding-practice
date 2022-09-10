351dae9a68906582f0fcacc7430d71f7887250a4
Diego Velasquez <iorivilla@hotmail.com>
2022-09-11 09:19:04 +1000

    Add Functions and Method Practice

--- modern_java_course_ninja/functions-and-methods/practice/practice.js
+++ modern_java_course_ninja/functions-and-methods/practice/practice.js
@@ -24,4 +24,93 @@ const calcuNum = radius => {
 
 calcuNum(2);
 
-console.log('<-------- function expression ---------->');
+console.log('<-------- Returning Values with arrow functions ---------->');
+const myAreaCalc = elradio => {
+    let miarea = 3.1415 * elradio**2;
+    return miarea;
+};
+
+const areaResult = myAreaCalc(5)
+console.log(areaResult);
+
+console.log('<-------- Returning Values, return simple version ---------->');
+const correctAera = myarea => {
+    return 3.1415 * myarea**2;
+}
+
+const resultCorrectArea = correctAera(5);
+console.log(resultCorrectArea);
+
+console.log('<-------- Returning Values, arrow function simple version ---------->');
+const perfectArea = fullarea => 3.1415 * fullarea**2;
+
+const resultPerfectArea = perfectArea(5);
+console.log(resultPerfectArea);
+
+const saludando = () => "hola a todos";
+const miSaludo = saludando();
+console.log(miSaludo);
+
+const miFactura = (producto, impuesto) => {
+    let totalProd = 0;
+    for(let i = 0; i < producto.length; i++) {
+        totalProd = producto[i] + producto[i] * impuesto;
+    };
+    return totalProd;
+};
+
+console.log(miFactura([10,15,30], 0.2));
+
+console.log('<-------- For each Method and Call Back ---------->');
+const myLlamada = llamando => {
+    let llamadaValor = 40;
+    llamando(llamadaValor);
+}
+
+myLlamada(miValor => {
+    console.log(miValor);
+});
+
+const miGente = ['Necia', 'George', 'Manolo', 'Alba', 'Carlos'];
+miGente.forEach((persona, numero) => {
+    console.log(`${numero} Hola senor(@) ${persona}`);
+});
+
+console.log('<-------- For each Method inserting html ---------->');
+const genteGeneral = ['Necia', 'George', 'Manolo', 'Alba', 'Carlos'];
+const listaGente = document.querySelector('.mi-gente');
+
+html = ``;
+genteGeneral.forEach(lapersona => {
+    html += `
+        <li style="color: white; background: brown; border-bottom: 1px solid white; list-style: none; padding: 10px 20px;">${lapersona}</li>
+    `;
+});
+
+console.log(html);
+listaGente.innerHTML = html;
+
+const nombresPersonas = ['Necia', 'George', 'Manolo', 'Alba', 'Carlos'];
+const myUlLista = document.querySelector('.nombres');
+
+miHtml = ``;
+nombresPersonas.forEach(nombre => {
+    miHtml += `
+        <li style="background: #4d4d4d; color: white; padding: 10px 20px; margin-bottom: 5px;">${nombre}</li>
+    `;
+});
+
+console.log(miHtml);
+myUlLista.innerHTML = miHtml;
+
+const miListaNombres = ['Jessica', 'Alba', 'Carlos', 'Sebastian', 'Benji'];
+const listaEnUl = document.querySelector('.nomb-lista');
+
+html = ``;
+miListaNombres.forEach(elNombre => {
+    html += `
+        <li style="Background: orange; margin-bottom: 10px; padding: 25px 10px;">${elNombre}</li>
+    `;
+});
+
+listaEnUl.innerHTML = html;
