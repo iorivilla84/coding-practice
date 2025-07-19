ce5155f5785c7f4d641f1a862e6d3b5c9d28ecee
Diego Velasquez <{ID}+{username}@users.noreply.github.com>
2025-07-19 17:19:28 +1000

    Add HTTP request Methods and JSON data objects

---
+++ code-academy-course/javaScript/challenges/movie-finder-http-request/js/filmFinder.js
@@ -0,0 +1,85 @@
+import {
+    populateGenreDropdown,
+    getSelectedGenre,
+    clearCurrentMovie,
+    getRandomMovie,
+    displayMovie
+} from './helpers/viewHelpers.js'
+
+const tmdbKey = VITE_API_KEY;
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list'
+  const requestParams = `?api_key=${tmdbKey}`
+
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json()
+    const movies = jsonResponse.results;
+    return movies
+  }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getMovieInfo = async (movie) => {
+  const movieId = movie.id;
+  const movieEndpoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch);
+    console.log(response)
+    if (response.ok) {
+      const movieInfo = await response.json();
+      return movieInfo;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = async () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info);
+};
+
+const initFilmFinder = () => {
+    getGenres().then(populateGenreDropdown);
+    playBtn.onclick = showRandomMovie;
+}
+
+export { initFilmFinder, showRandomMovie };
---
+++ code-academy-course/javaScript/challenges/movie-finder-http-request/js/main.js
@@ -0,0 +1,7 @@
+import { initFilmFinder } from "./filmFinder.js";
+
+const init = () => {
+    initFilmFinder()
+}
+
+document.addEventListener('DOMContentLoaded', init)
